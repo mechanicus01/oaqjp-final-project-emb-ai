034983b48f02a7c5653bc201cb6245006915eca2
mechanicus01 <mechanicus01@gmail.com>
2025-06-19 17:05:29 -0400

    formatted the response

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,10 +1,21 @@
 import requests, json
 
-
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     input_json = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=input_json, headers=header)
     formatted_response = json.loads(response.text)
-    return formatted_response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+
+    formatted_response = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
+    print(json.dumps(formatted_response, indent=4))    
+    #return formatted_response
