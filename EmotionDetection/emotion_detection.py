ea330f7f3d3a97c6e96f6d13c9e765ac235ae1fc
mechanicus01 <mechanicus01@gmail.com>
2025-06-24 22:35:23 -0400

    Add Error Handling for blank entries

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,6 +5,16 @@ def emotion_detector(text_to_analyse):
     input_json = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=input_json, headers=header)
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
+    
     formatted_response = json.loads(response.text)
     emotions = formatted_response['emotionPredictions'][0]['emotion']
     dominant_emotion = max(emotions, key=emotions.get)
@@ -17,5 +27,5 @@ def emotion_detector(text_to_analyse):
         'sadness': emotions['sadness'],
         'dominant_emotion': dominant_emotion
     }
-    print(json.dumps(formatted_response, indent=4))    
+    #print(json.dumps(formatted_response, indent=4))    
     return formatted_response
--- server.py
+++ server.py
@@ -11,14 +11,15 @@ def render_index_page():
 def emotion_detect():
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
-
-    return (
-        f"For the given statement, the system response is '{response['dominant_emotion']}': {response['joy']},  "
-        f"'anger' {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, "
-        f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
-        f"The dominant emotion is {response['dominant_emotion']}."
-    )
-
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+    else:
+        return (
+            f"For the given statement, the system response is '{response['dominant_emotion']}': {response['joy']},  "
+            f"'anger' {response['anger']}, 'disgust': {response['disgust']}, 'fear': {response['fear']}, "
+            f"'joy': {response['joy']} and 'sadness': {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}."
+        )
 # Run the app
 if __name__ == "__main__":
     server.run(debug=True)
